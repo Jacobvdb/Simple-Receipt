d6cbd15f5b0353f869d9f4b001823e41a4f759a2
jacobvdb <jacob@bkper.com>
2021-08-04 11:47:38 -0300

    bring more parameters to the screen

--- Code.ts
+++ Code.ts
@@ -1,8 +1,11 @@
 function doGet(e) {
     var bookId = e.parameter.bookId;
     var query = e.parameter.query;
+    var dselect = e.parameter.dselect;
+    var transactionId = e.parameter.transactionId;
+
     var htmlTemplate = HtmlService.createTemplateFromFile('Dialog');
-    htmlTemplate.dataFromServerTemplate = { bookid: bookId, query: query };
+    htmlTemplate.dataFromServerTemplate = { bookid: bookId, query: query,dselect: dselect ,transactionId: transactionId };
     var htmlOutput = htmlTemplate.evaluate().setSandboxMode(HtmlService.SandboxMode.IFRAME)
         .setTitle('Test App');
     return htmlOutput;
@@ -11,7 +14,7 @@ function doGet(e) {
 
 
 function initialize(bookId){
-  var book = BkperApp.openById(bookId)
+  var book = BkperApp.openById(bookId,query, dselect, transactionid)
   var bookName = book.getName();
   return bookName
 
