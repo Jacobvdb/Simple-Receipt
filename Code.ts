b599fcc525733309371f195352f0141cd4b6cf8a
jacobvdb <jacob@bkper.com>
2021-08-06 11:20:06 -0300

    making it work

--- Code.ts
+++ Code.ts
@@ -1,23 +1,38 @@
+var __assign = (this && this.__assign) || function () {
+  __assign = Object.assign || function (t) {
+      for (var s, i = 1, n = arguments.length; i < n; i++) {
+          s = arguments[i];
+          for (var p in s)
+              if (Object.prototype.hasOwnProperty.call(s, p))
+                  t[p] = s[p];
+      }
+      return t;
+  };
+  return __assign.apply(this, arguments);
+};
+
 function doGet(e) {
     var bookId = e.parameter.bookId;
     var transactionIds = e.parameter.transactionIds;
-    var templateUrl = e.parameter.templateUrl;
-    
 
-    
-    
+    if (transactionIds){
     var htmlTemplate = HtmlService.createTemplateFromFile('Dialog');
-    htmlTemplate.dataFromServerTemplate = { bookid: bookId, transactionIds: transactionIds, templateUrl : templateUrl };
+    htmlTemplate.dataFromServerTemplate = { bookid: bookId, transactionIds: transactionIds, msg:""};
     var htmlOutput = htmlTemplate.evaluate().setSandboxMode(HtmlService.SandboxMode.IFRAME)
-        .setTitle('Test App');
-    return htmlOutput;
-    
+        .setTitle('Receipt');
+    } else{
+      var htmlTemplate = HtmlService.createTemplateFromFile('Dialog');
+      htmlTemplate.dataFromServerTemplate = { bookid: bookId, transactionIds: transactionIds, msg:"Please select some transaction(s) to put on the receipt."};
+      var htmlOutput = htmlTemplate.evaluate().setSandboxMode(HtmlService.SandboxMode.IFRAME)
+          .setTitle('Instructions');  
+    }
+    return htmlOutput;    
 }
 
 
-function initialize(bookId, transactionIds, templateUrl){
+function initialize(bookId, transactionIds){
   var book = BkperApp.openById(bookId)
-  var bookName = book.getName();
+ 
 
   const txIds = transactionIds.split(" ");
   Logger.log("txids:" + txIds)
@@ -29,47 +44,69 @@ function initialize(bookId, transactionIds, templateUrl){
     let tx = book.getTransaction(txId)
     Logger.log("quantity:" + tx.getAmount())
 
+    
 
   } 
 
-
+  var model = generateModel(book, transactionIds);
+  var object = merge(model);
+  var receiptUrl = object.receiptUrl;
+  var receiptName = object.receiptName;
   
-  return {bookName, bookId,   transactionIds, templateUrl}
+  return { bookId,   transactionIds, receiptUrl, receiptName}
 
 }
 
 
 
-function generateModel(book, customerName, afterDate, beforeDate) {
+function generateModel(book, transactionIds) {
   // Book Properties
   //var book = BkperApp.openById(bookId);
   var model = {
       book: __assign(__assign({}, book.getProperties()), { name: book.getName() })
   };
   // Account properties
-  var account = book.getAccount(customerName);
-  model.customer = __assign(__assign({}, account.getProperties()), { name: account.getName(), balance: account.getBalance() });
+  //var account = book.getAccount(customerName);
+  //model.customer = __assign(__assign({}, account.getProperties()), { name: account.getName(), balance: account.getBalance() });
       
-  Logger.log( "account:'"+ customerName + "' after:" + afterDate + " before:"+beforeDate)
+  //Logger.log( "account:'"+ customerName + "' after:" + afterDate + " before:"+beforeDate)
   // transactions
+  const transactionIdsArray = transactionIds.split(" ");
+  var total = 0*1;
   model.transactions = [];
-  var transactionIterator = book.getTransactions("account:'"+ customerName + "' after:" + afterDate + " before:"+beforeDate );
-  while (transactionIterator.hasNext()) {
-      var transaction = transactionIterator.next();
+  for (var i = 0; i < transactionIdsArray.length; i++) {
+      let transactionId = transactionIdsArray[i];
+      let transaction = book.getTransaction(transactionId);
       model.transactions.push({
-          date: transaction.getInformedDateText(),
-          description: transaction.getDescription(),
-          account: transaction.getCreditAccount().getName(),
-          amount: transaction.getAmount()
+         date: transaction.getInformedDateText(),
+         description: transaction.getDescription(),
+         account: transaction.getCreditAccount().getName(),
+         amount: transaction.getAmount()
       });
-  }
-  return model;
+      total +=  Number(transaction.getAmount());
+  } 
+  //Receipt
+  model.receipt = __assign({ total: total ,date :Utilities.formatDate(new Date(),Session.getScriptTimeZone(), 'dd/MM/yyyy HH:mm:ss')});
+
+
+
+ Logger.log(model.receipt.total  + " " + total )
+ return model;
+}
+
+
+function testGenerateModel(){
+  var bookId = "agtzfmJrcGVyLWhyZHITCxIGTGVkZ2VyGICAoITsoaMJDA"
+  var transactionIds = "1c13381a-ecc4-4db7-a4ab-48cf19c983d2 3a5b72a6-a4bf-48f6-9e87-c18baf1d9dad"
+  var book = BkperApp.openById(bookId)
+   generateModel(book, transactionIds)
 }
 
 
 function merge(model) {
+  let folderId = model.book.receipt_folder_id;
   var params = {   
-      'template': "https://docs.google.com/document/d/16FBd36RzL3Xqk9tDL5pcTRadT043baJ_JfgY-6F0HiE/edit",
+      'template': model.book.receipt_template,
       'model': model,
       'format': 'pdf'
   };
@@ -79,21 +116,21 @@ function merge(model) {
       'payload': JSON.stringify(params),
       'muteHttpExceptions': false
   };
-  Logger.log("ok");
+  
   var response = UrlFetchApp.fetch('https://api.doxey.io/merge', options);
   var document = response.getBlob();
-  document.setName("Bkper Doxey GS sample.pdf");
-  var file =DriveApp.getFolderById("128bwrjur-8tgb2HxWmXFIC2WH_1xODsY").createFile(document);
-  var invoiceURL = file.getUrl();
-  return invoiceURL
+  var date = Utilities.formatDate(new Date(),Session.getScriptTimeZone(), 'dd-MM-yyyy')
+  var receiptName = "Receipt " + date+".pdf";
+  document.setName(receiptName);
+  var file =DriveApp.getFolderById(folderId).createFile(document);
+  var receiptUrl = file.getUrl();
+  return {receiptUrl, receiptName}
 }
 
-
-
-
-//${book.id} - the current book id
-//${book.properties.xxxxx} - any property value from the current book
-//${transactions.query} - the current query being executed on transactions list
-//${transactions.ids} - the ids of selected transactions, splitted by comma
-//${account.id} - the current account being filterd
-//${account.properties.xxxxx} - any property value from the current account being filtered
+function testMerge(){
+  var bookId = "agtzfmJrcGVyLWhyZHITCxIGTGVkZ2VyGICAoITsoaMJDA"
+  var transactionIds = "1c13381a-ecc4-4db7-a4ab-48cf19c983d2 3a5b72a6-a4bf-48f6-9e87-c18baf1d9dad"
+  var book = BkperApp.openById(bookId)
+  const model =  generateModel(book, transactionIds);
+  merge(model);
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-This test app took me through all the steps to create a very simple Bkper App
+Simple Receipt for Bkper 
+
+Generates a simple pdf receipt based on selected transactions on your Bkper book. 
+
+Requirements 
+Book Properties 
+receipt_folder_id: the id of the Google Drive Folder where you store your receipts
+receipt_template: the url to the Google Doc template for your receipts
+
+
+
 
