f2934ed1397b7fad6aac98aa00a15cc605ce4e3f
jacobvdb <jacob@bkper.com>
2024-05-29 13:58:46 -0300

    correction on the merge

--- Code.ts
+++ Code.ts
@@ -1,4 +1,5 @@
 // Compiled using ts2gas 3.6.4 (TypeScript 4.2.3)
+// Compiled using ts2gas 3.6.4 (TypeScript 4.2.3)
 var __assign = (this && this.__assign) || function () {
     __assign = Object.assign || function (t) {
         for (var s, i = 1, n = arguments.length; i < n; i++) {
@@ -11,14 +12,10 @@ var __assign = (this && this.__assign) || function () {
     };
     return __assign.apply(this, arguments);
 };
- 
-
 function doGet(e) {
     var bookId = e.parameter.bookId;
     var transactionIds = e.parameter.transactionIds;
-    
     var propertiesObj = checkProperties(bookId);
-     
     if (propertiesObj.msg == "") {
         if (transactionIds) {
             var htmlTemplate = HtmlService.createTemplateFromFile('Dialog');
@@ -38,19 +35,16 @@ function doGet(e) {
     else {
         // Something wrong with the properties 
         var htmlTemplate = HtmlService.createTemplateFromFile('Dialog');
-        htmlTemplate.dataFromServerTemplate = { bookid: bookId, transactionIds: transactionIds, msg: propertiesObj.msg, templateUrl: propertiesObj.templateUrl  };
+        htmlTemplate.dataFromServerTemplate = { bookid: bookId, transactionIds: transactionIds, msg: propertiesObj.msg, templateUrl: propertiesObj.templateUrl };
         var htmlOutput = htmlTemplate.evaluate().setSandboxMode(HtmlService.SandboxMode.IFRAME)
             .setTitle('Instructions');
     }
     return htmlOutput;
 }
-
-
 function checkProperties(bookId) {
     var book = BkperApp.openById(bookId);
     var properties = book.getProperties();
     var msg = "";
-    
     //Receipt template
     // Check content of the property
     if (!properties.receipt_template_url) {
@@ -60,6 +54,10 @@ function checkProperties(bookId) {
         var templateUrl = properties.receipt_template_url;
     }
     //Check for access to receipt_template
+    //DocumentApp.openByUrl(templateUrl)
+   // const doc = DocumentApp.openByUrl(templateUrl);
+   // const id = doc.getId();
+
     var templateId = getIdFromUrl(templateUrl);
     try {
         DriveApp.getFileById(templateId);
@@ -67,51 +65,43 @@ function checkProperties(bookId) {
     catch (e) {
         msg = "Please check the book property receipt_template_url <br><br>" + e;
     }
-
     //Doxey Api Key
     if (!properties.doxey_api_key) {
         // The document will use the watermark
-        var doxeyApiKey ="";
-    } else {
+        var doxeyApiKey = "";
+    }
+    else {
         doxeyApiKey = properties.doxey_api_key;
-    
         // check the license
-        var  params = { 'apiKey': doxeyApiKey };
+        var params = { 'apiKey': doxeyApiKey };
         var options = {
             'contentType': "application/json",
             'method': 'post',
             'payload': JSON.stringify(params),
             'muteHttpExceptions': false
         };
-        try{
+        try {
             UrlFetchApp.fetch('https://api.doxey.io/license', options);
             doxeyApiKey = "ok";
-        } catch (e){
-          msg = "Please check the book property doxey_api_key <br><br>" + e;
+        }
+        catch (e) {
+            msg = "Please check the book property doxey_api_key <br><br>" + e;
         }
     }
-    
-    
-    return { msg, templateUrl,  doxeyApiKey };
+    return { msg, templateUrl, doxeyApiKey };
 }
-
-
 // Initialize the Pop up in the book.
-function initialize(bookId, transactionIds, msg, templateUrl,  doxeyApiKey) {
+function initialize(bookId, transactionIds, msg, templateUrl, doxeyApiKey) {
     var book = BkperApp.openById(bookId);
-
-    if (doxeyApiKey =="ok"){
-       doxeyApiKey = book.getProperty("doxey_api_key")
-    } 
+    if (doxeyApiKey == "ok") {
+        doxeyApiKey = book.getProperty("doxey_api_key");
+    }
     // create the model 
     var model = generateModel(book, transactionIds);
-    
     // get the document form doxey
-    var document = merge(model, templateUrl,  doxeyApiKey);
-    return {document}
+    var document = merge(model, templateUrl, doxeyApiKey);
+    return { document };
 }
-
-
 // Generation of the model for the receipt
 function generateModel(book, transactionIds) {
     // Book Properties
@@ -122,7 +112,6 @@ function generateModel(book, transactionIds) {
     // Account properties
     //var account = book.getAccount(customerName);
     //model.customer = __assign(__assign({}, account.getProperties()), { name: account.getName(), balance: account.getBalance() });
-
     // transactions
     const transactionIdsArray = transactionIds.split(" ");
     var total = 0 * 1;
@@ -142,23 +131,25 @@ function generateModel(book, transactionIds) {
     model.receipt = __assign({ total: total, date: Utilities.formatDate(new Date(), Session.getScriptTimeZone(), 'dd/MM/yyyy HH:mm:ss') });
     return model;
 }
-
-
 // Create the receipt pdf
-function merge(model, templateUrl,  doxeyApiKey) {
-
-    if(doxeyApiKey == ""){
-       var params = {
-           'template': templateUrl,
-           'model': model,
-           'format': 'pdf' 
-       };
-    } else{
+function merge(model, templateUrl, doxeyApiKey) {
+  Logger.log(templateUrl)
+    if (doxeyApiKey == "") {
+      Logger.log("no key");
         var params = {
             'template': templateUrl,
             'model': model,
             'format': 'pdf',
-            'apiKey': doxeyApiKey  // no watermark
+            'apiKey':''
+        };
+    }
+    else {
+         Logger.log("with key");
+        var params = {
+            'template': templateUrl,
+            'model': model,
+            'format': 'pdf',
+            'apiKey': doxeyApiKey // no watermark
         };
     }
     var options = {
@@ -168,39 +159,52 @@ function merge(model, templateUrl,  doxeyApiKey) {
         'muteHttpExceptions': false
     };
     var response = UrlFetchApp.fetch('https://api.doxey.io/merge', options);
+    //var document = response.getBlob();
+    //document.setName("Bkper Doxey Gs sample.pdf");
+    //DriveApp.getFolderById("1543DzFsTzXco2z34bpdKkdGbPu42tkjl").createFile(document)
+    //
     var blob = response.getBlob();
-    var bytes= blob.getBytes();
+    var bytes = blob.getBytes();
     var encoded = Utilities.base64Encode(bytes);
-    
-    return encoded
+    return encoded;
 }
-
 // Utilities
 // extract id from url for folder and docs
 function getIdFromUrl(url) { return url.match(/[-\w]{25,}/); }
-
 // Tests
 // test the property check 
 function testCheckProperties() {
     var bookId = "agtzfmJrcGVyLWhyZHITCxIGTGVkZ2VyGICAoITsoaMJDA";
-}
 
+    checkProperties(bookId)
+}
 // test the generation of th model for the receipt
 function testGenerateModel() {
     var bookId = "agtzfmJrcGVyLWhyZHITCxIGTGVkZ2VyGICAoITsoaMJDA";
-    var transactionIds = "1c13381a-ecc4-4db7-a4ab-48cf19c983d2 3a5b72a6-a4bf-48f6-9e87-c18baf1d9dad";
+    var transactionIds = "1c13381a-ecc4-4db7-a4ab-48cf19c983d2 3a5b72a6-a4bf-48f6-9e87-c18baf1d9dad ";
     var book = BkperApp.openById(bookId);
     generateModel(book, transactionIds);
 }
-
 // test the creation of the receipt
 function testMerge() {
     var bookId = "agtzfmJrcGVyLWhyZHITCxIGTGVkZ2VyGICAoITsoaMJDA";
-    var transactionIds = "1c13381a-ecc4-4db7-a4ab-48cf19c983d2 3a5b72a6-a4bf-48f6-9e87-c18baf1d9dad";
+    var transactionIds = "1c13381a-ecc4-4db7-a4ab-48cf19c983d2 3a5b72a6-a4bf-48f6-9e87-c18baf1d9dad 29aac2a5-d5c5-4dbc-a98d-9eacd6312ef4";
     var folderId = "13d36TqAJ8sLdevN95FhK7jyXblBv_Grz";
-    var doxeyApiKey ="";
-    var templateUrl ="https://docs.google.com/document/d/1MMENpgkJu24RqHDtVvn9jEJRcEgBo_KtND123VFNTnk/edit";
+    var doxeyApiKey = "";
+    var templateUrl = "https://docs.google.com/document/d/1MMENpgkJu24RqHDtVvn9jEJRcEgBo_KtND123VFNTnk/edit";
     var book = BkperApp.openById(bookId);
     const model = generateModel(book, transactionIds);
     merge(model, templateUrl, folderId, doxeyApiKey);
 }
+
+
+
+function testregex( ){
+  var url = "https://docs.google.com/document/d/1MMENpgkJu24RqHDtVvn9jEJRcEgBo_KtND123VFNTnk/edit"
+  Logger.log(url.match(/[-\w]{25,}/))
+  var id =url.match(/[-\w]{25,}/)
+
+  DriveApp.getFileById('1MMENpgkJu24RqHDtVvn9jEJRcEgBo_KtND123VFNTnk')
+}
+
+
