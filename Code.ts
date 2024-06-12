df1273f1345baae9d7d264a9cad6dbb895a89cec
jacobvdb <jacob@bkper.com>
2024-06-12 17:38:59 -0300

    Cleaning shoelace jquery

--- Code.ts
+++ Code.ts
@@ -1,132 +1,135 @@
 // Compiled using ts2gas 3.6.4 (TypeScript 4.2.3)
-// Compiled using ts2gas 3.6.4 (TypeScript 4.2.3)
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function (t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s)
-                if (Object.prototype.hasOwnProperty.call(s, p))
-                    t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
+
 function doGet(e) {
-    var bookId = e.parameter.bookId;
-    var transactionIds = e.parameter.transactionIds;
-    var propertiesObj = checkProperties(bookId);
-    if (propertiesObj.msg == "") {
-        if (transactionIds) {
-            var htmlTemplate = HtmlService.createTemplateFromFile('Dialog');
-            htmlTemplate.dataFromServerTemplate = { bookid: bookId, transactionIds: transactionIds, msg: propertiesObj.msg, templateUrl: propertiesObj.templateUrl, doxeyApiKey: propertiesObj.doxeyApiKey };
-            var htmlOutput = htmlTemplate.evaluate().setSandboxMode(HtmlService.SandboxMode.IFRAME)
-                .setTitle('Receipt');
-        }
-        else {
-            // No transactions selected
-            var htmlTemplate = HtmlService.createTemplateFromFile('Dialog');
-            var msg = "Please select some transaction(s) to put on the receipt.";
-            htmlTemplate.dataFromServerTemplate = { bookid: bookId, transactionIds: transactionIds, msg: msg };
-            var htmlOutput = htmlTemplate.evaluate().setSandboxMode(HtmlService.SandboxMode.IFRAME)
-                .setTitle('Instructions');
-        }
-    }
-    else {
-        // Something wrong with the properties 
-        var htmlTemplate = HtmlService.createTemplateFromFile('Dialog');
-        htmlTemplate.dataFromServerTemplate = { bookid: bookId, transactionIds: transactionIds, msg: propertiesObj.msg, templateUrl: propertiesObj.templateUrl };
-        var htmlOutput = htmlTemplate.evaluate().setSandboxMode(HtmlService.SandboxMode.IFRAME)
-            .setTitle('Instructions');
-    }
-    return htmlOutput;
-}
+    if (!e || !e.parameter || !e.parameter.bookId || !e.parameter.transactionIds) {
+        // Handle the case when the bookId or transactionIds parameter is missing
+        var msg = "Missing bookId or transactionIds parameter";
+        var status = 400; 
+        //return ContentService.createTextOutput("Missing bookId or transactionIds parameter");
+    } else {
+        setUserProperty("bookId", e.parameter.bookId);
+        setUserProperty("transactionIds", e.parameter.transactionIds);
 
-function checkProperties(bookId) {
-    var book = BkperApp.openById(bookId);
-    var properties = book.getProperties();
-    var msg = "";
-
-    //Receipt template
-    // Check content of the property
-    if (!properties.receipt_template_url) {
-        var templateUrl = "https://docs.google.com/document/d/1MMENpgkJu24RqHDtVvn9jEJRcEgBo_KtND123VFNTnk/edit";
-    }
-    else {
-        var templateUrl = properties.receipt_template_url;
-    }
+        var book = BkperApp.openById(e.parameter.bookId);
+        var bookName = book.getName()
+        setUserProperty("bookName", book.getName());
+        setUserProperty("bookDatePattern", book.getDatePattern());
+        setUserProperty("bookTimeZone", book.getTimeZone());
     
-    //Doxey Api Key
-    if (!properties.doxey_api_key) {
-        // The document will use the watermark
-        var doxeyApiKey = "";
+        var msg = "Preparing your receipt";
+        var status = 200;
     }
-    else {
-        doxeyApiKey = properties.doxey_api_key;
-        // check the license
-        var params = { 'apiKey': doxeyApiKey };
-        var options = {
-            'contentType': "application/json",
-            'method': 'post',
-            'payload': JSON.stringify(params),
-            'muteHttpExceptions': false
-        };
-        try {
-            UrlFetchApp.fetch('https://api.doxey.io/license', options);
-            doxeyApiKey = "ok";
-        }
-        catch (e) {
-            msg = "Please check the book property doxey_api_key <br><br>" + e;
-        }
-    }
-    return { msg, templateUrl, doxeyApiKey };
+    var htmlTemplate = HtmlService.createTemplateFromFile('Addon');
+    htmlTemplate.dataFromServerTemplate = { bookName: bookName, msg: msg , status: status};
+    var htmlOutput = htmlTemplate.evaluate().setSandboxMode(HtmlService.SandboxMode.IFRAME)
+    .setTitle('Generate Receipt');
+
+return htmlOutput;
 }
 
 // Initialize the Pop up in the book.
-function initialize(bookId, transactionIds, msg, templateUrl, doxeyApiKey) {
-    var book = BkperApp.openById(bookId);
-    if (doxeyApiKey == "ok") {
-        doxeyApiKey = book.getProperty("doxey_api_key");
-    }
+function getReceipt() {
+   
+    var bookId = getUserProperty("bookId");
+    var book = BkperApp.getBook(bookId);
+    var transactionIds = getUserProperty("transactionIds");
+     
     // create the model 
     var model = generateModel(book, transactionIds);
+    var templateUrl = model.book.receipt_template_url || "https://docs.google.com/document/d/1MMENpgkJu24RqHDtVvn9jEJRcEgBo_KtND123VFNTnk/edit";
+    var doxeyApiKey = checkDoxeyApiKey(model.book.doxey_api_key) ;
+
+    
+    // in case there is no receipt_number book property, create and define one
+    if (!model.book.receipt_number) {
+        book.setProperty("receipt_number", "1001");
+        book.update();
+        model.book.receipt_number = "1001";
+    } 
+
     // get the document form doxey
     var document = merge(model, templateUrl, doxeyApiKey);
-    return { document };
+    
+
+    // Increment the receipt number
+    incrementReceiptNumber(book);
+    // cleanup properties
+    setUserProperty("bookId", "")
+    setUserProperty("transactionIds", "")
+    book.update();
+
+
+   return { document };
 }
 
-// Generation of the model for the receipt
 function generateModel(book, transactionIds) {
-    
-    var model = {
-        book: __assign(__assign({}, book.getProperties()), { name: book.getName() })
-    };
-    
-    // transactions
     const transactionIdsArray = transactionIds.split(" ");
-    var total = 0 * 1;
-    model.transactions = [];
+   
+    // Initialize the model object
+    var model = {
+        transactions: [],
+        receipt: {
+            total: 0,
+            date: new Date().toLocaleString()
+        },
+        fromAccount: {},
+        toAccount: {},
+        book: book.getProperties() // Include all book properties
+    };
+
     for (var i = 0; i < transactionIdsArray.length; i++) {
         let transactionId = transactionIdsArray[i];
         let transaction = book.getTransaction(transactionId);
-        model.transactions.push({
+        let properties = transaction.getProperties();
+
+        // Fetch credit and debit accounts
+        let creditAccount = transaction.getCreditAccount();
+        let debitAccount = transaction.getDebitAccount();
+        
+        // Fetch account properties
+        let creditAccountProps = creditAccount.getProperties();
+        let debitAccountProps = debitAccount.getProperties();
+
+        // Set fromAccount properties if not already set
+        if (i === 0) { // Assuming the first transaction's accounts are representative
+            model.fromAccount = creditAccountProps;
+            model.toAccount = debitAccountProps;
+        }
+
+        // Create a transaction object
+        let transactionObject = {
             date: transaction.getInformedDateText(),
             description: transaction.getDescription(),
-            account: transaction.getCreditAccount().getName(),
+            fromAccount: creditAccount.getName(),
+            toAccount: debitAccount.getName(),
             amount: transaction.getAmount().toFixed(2)
-        });
-        total += Number(transaction.getAmount());
+        };
+
+        // Add transaction properties as key-value pairs
+        for (var key in properties) {
+            if (properties.hasOwnProperty(key)) {
+                transactionObject[key] = properties[key];
+            }
+        }
+
+        // Update receipt total
+        model.receipt.total += parseFloat(transaction.getAmount());
+
+        // Push the transaction object into the model.transactions array
+        model.transactions.push(transactionObject);
     }
-    //Receipt
-    model.receipt = __assign({ total: total.toFixed(2), date: Utilities.formatDate(new Date(), Session.getScriptTimeZone(), 'dd/MM/yyyy HH:mm:ss') });
+
+    // Format the receipt total to two decimal places
+    model.receipt.total = model.receipt.total.toFixed(2);
+    Logger.log(model);
+    
     return model;
 }
 
+
 // Create the receipt pdf
 function merge(model, templateUrl, doxeyApiKey) {
-  Logger.log(templateUrl)
     if (doxeyApiKey == "") {
-      Logger.log("no key");
         var params = {
             'template': templateUrl,
             'model': model,
@@ -135,7 +138,6 @@ function merge(model, templateUrl, doxeyApiKey) {
         };
     }
     else {
-         Logger.log("with key");
         var params = {
             'template': templateUrl,
             'model': model,
@@ -150,49 +152,72 @@ function merge(model, templateUrl, doxeyApiKey) {
         'muteHttpExceptions': false
     };
     var response = UrlFetchApp.fetch('https://api.doxey.io/merge', options);
-   
-    var blob = response.getBlob();
-    var bytes = blob.getBytes();
+    var document = response.getBlob();
+    var bytes = document.getBytes();
     var encoded = Utilities.base64Encode(bytes);
     return encoded;
 }
 
 
 
-// Tests
-// test the property check 
-function testCheckProperties() {
-    var bookId = "agtzfmJrcGVyLWhyZHITCxIGTGVkZ2VyGICAoITsoaMJDA";
-
-    checkProperties(bookId)
+function setUserProperty(propertyKey, propertyValue) {
+    var userProperties = PropertiesService.getUserProperties();
+    userProperties.setProperty(propertyKey, propertyValue);
+    return;
 }
-// test the generation of th model for the receipt
+
+function getUserProperty(propertyKey) {
+    var userProperties = PropertiesService.getUserProperties();
+    var propertyValue = userProperties.getProperty(propertyKey);
+    return propertyValue;
+}
+
+function checkDoxeyApiKey(doxeyApiKey){
+    if (doxeyApiKey) {
+        try {
+            var params = { 'apiKey': doxeyApiKey };
+            var options = {
+                'contentType': "application/json",
+                'method': 'post',
+                'payload': JSON.stringify(params),
+                'muteHttpExceptions': false
+            };
+            UrlFetchApp.fetch('https://api.doxey.io/license', options);
+        } catch (e) {
+            msg = "Please check the book property doxey_api_key <br><br>" + e;
+            doxeyApiKey = "";
+        }
+  
+        return doxeyApiKey
+    } else { return ""}
+}
+
+function incrementReceiptNumber(book) {
+    var currentNumber = parseInt(book.getProperty("receipt_number"));
+    book.setProperty("receipt_number", (currentNumber + 1).toString());
+    book.update();
+}
+
+
+function closeThis(){
+    Logger.log("closing")
+    return
+}
+
+// test the generation of the model for the receipt
 function testGenerateModel() {
     var bookId = "agtzfmJrcGVyLWhyZHITCxIGTGVkZ2VyGICAoITsoaMJDA";
-    var transactionIds = "1c13381a-ecc4-4db7-a4ab-48cf19c983d2 3a5b72a6-a4bf-48f6-9e87-c18baf1d9dad ";
+    var transactionIds = "84498fd2-bc90-4743-97ce-ef04a6a0b133 8a3a48e4-b729-4a0c-a62a-75dbfce52980";
     var book = BkperApp.openById(bookId);
     generateModel(book, transactionIds);
 }
 // test the creation of the receipt
 function testMerge() {
     var bookId = "agtzfmJrcGVyLWhyZHITCxIGTGVkZ2VyGICAoITsoaMJDA";
-    var transactionIds = "1c13381a-ecc4-4db7-a4ab-48cf19c983d2 3a5b72a6-a4bf-48f6-9e87-c18baf1d9dad 29aac2a5-d5c5-4dbc-a98d-9eacd6312ef4";
+    var transactionIds = "84498fd2-bc90-4743-97ce-ef04a6a0b133 8a3a48e4-b729-4a0c-a62a-75dbfce52980";
     var folderId = "13d36TqAJ8sLdevN95FhK7jyXblBv_Grz";
     var doxeyApiKey = "";
     var templateUrl = "https://docs.google.com/document/d/1MMENpgkJu24RqHDtVvn9jEJRcEgBo_KtND123VFNTnk/edit";
     var book = BkperApp.openById(bookId);
     const model = generateModel(book, transactionIds);
     merge(model, templateUrl, folderId, doxeyApiKey);
-}
-
-
-
-function testregex( ){
-  var url = "https://docs.google.com/document/d/1MMENpgkJu24RqHDtVvn9jEJRcEgBo_KtND123VFNTnk/edit"
-  Logger.log(url.match(/[-\w]{25,}/))
-  var id =url.match(/[-\w]{25,}/)
-
-  DriveApp.getFileById('1MMENpgkJu24RqHDtVvn9jEJRcEgBo_KtND123VFNTnk')
-}
-
-
